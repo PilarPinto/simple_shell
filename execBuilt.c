8a0f2045eedb4c56529227adc7f35d283df0e5ad
PilarPinto <30970686+PilarPinto@users.noreply.github.com>
2019-11-27 18:20:19 -0500

    [Feature] - The fork process

--- execBuilt.c
+++ execBuilt.c
@@ -34,3 +34,37 @@ char **splitInput(char *array)
 	space[i] = NULL;
 	return (space);
 }
+
+
+void duplicateProcess(char *array, char **space)
+{
+	pid_t my_pid;
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid > 0)
+	{
+		wait(&status);
+	}
+	else if(pid == -1)
+	{
+		perror("Error:");
+		free(array);
+		free(space);
+		exit(0);
+	}
+	else if (pid == 0)
+	{
+		if ((execve(space[0], space, NULL) == -1))
+		{
+			perror("Does not execute, write valid command");
+			free(array);
+			free(space);
+			exit(0);
+		}
+
+	}
+
+}
