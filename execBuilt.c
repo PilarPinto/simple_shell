1a9a3ef2ff6b12c015c80d76e04f4b6d43046bf2
arcewp15 <970@holbertonschool.com>
2019-11-27 22:23:20 -0500

    [Update] identation ok, fix style of the code

--- execBuilt.c
+++ execBuilt.c
@@ -9,31 +9,31 @@ char **splitInput(char *array)
 	int counter = 0, i = 0;
 	char *tokenC;
 	char *token;
-        char **space;
-        char *arrayCopy;
+	char **space;
+	char *arrayCopy;
 
-        arrayCopy = strdup(array);
+	arrayCopy = strdup(array);
 	tokenC = strtok(arrayCopy, " \n\t");
 
-        arrayCopy = _strdup(array);
+	arrayCopy = _strdup(array);
 
 	while (tokenC != NULL)
-        {
-                tokenC = strtok(NULL, " \n\t");
-                counter++;
-        }
-        free(arrayCopy);
+	{
+		tokenC = strtok(NULL, " \n\t");
+		counter++;
+	}
+	free(arrayCopy);
 
 	space = malloc(sizeof(char *) * (counter + 1));
 	token = strtok(array, " \n\t");
 
-        while (token != NULL)
-        {
-                space[i] = token;
-                token = strtok(NULL, " \n\t");
-                i++;
-        }
-        space[i] = NULL;
+	while (token != NULL)
+	{
+		space[i] = token;
+		token = strtok(NULL, " \n\t");
+		i++;
+	}
+	space[i] = NULL;
 	return (space);
 }
 /**
@@ -44,33 +44,33 @@ char **splitInput(char *array)
  */
 void duplicateProcess(char *array, char **space)
 {
-        pid_t my_pid;
-        pid_t pid;
-        int status;
+	pid_t my_pid;
+	pid_t pid;
+	int status;
 
-        pid = fork();
+	pid = fork();
 
-        if (pid > 0)
-        {
-                wait(&status);
-        }
-        else if (pid == -1)
-        {
-                perror("Error:");
-                free(array);
-                free(space);
-                exit(0);
-        }
-        else if (pid == 0)
-        {
-                if ((execve(space[0], space, NULL) == -1))
-                {
-                        perror("Does not execute, write valid command");
-                        free(array);
-                        free(space);
-                        exit(0);
-                }
-        }
+	if (pid > 0)
+	{
+		wait(&status);
+	}
+	else if (pid == -1)
+	{
+		perror("Error:");
+		free(array);
+		free(space);
+		exit(0);
+	}
+	else if (pid == 0)
+	{
+		if ((execve(space[0], space, NULL) == -1))
+		{
+			perror("Does not execute, write valid command");
+			free(array);
+			free(space);
+			exit(0);
+		}
+	}
 }
 /**
  * enviromentShell - Gives the enviroment
@@ -79,18 +79,18 @@ void duplicateProcess(char *array, char **space)
  */
 void enviromentShell(char **space)
 {
-        char **environ;
-        int i = 0;
+	char **environ;
+	int i = 0;
 
-        if (_strcmp(space[0], "env") == 0)
-        {
-                while (environ[i] != NULL)
-                {
-                        write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-                        write(STDOUT_FILENO, "\n", strlen(environ[i]));
-                        i++;
-                }
-        }
+	if (_strcmp(space[0], "env") == 0)
+	{
+		while (environ[i] != NULL)
+		{
+			write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", strlen(environ[i]));
+			i++;
+		}
+	}
 }
 /**
  * exitof - Split the user input
@@ -100,10 +100,10 @@ void enviromentShell(char **space)
  */
 void exitof(char **space, char *array)
 {
-        if (_strcmp(space[0], "exit") == 0)
-        {
-                free(space);
-                free(array);
-                exit(0);
-        }
+	if (_strcmp(space[0], "exit") == 0)
+	{
+		free(space);
+		free(array);
+		exit(0);
+	}
 }
