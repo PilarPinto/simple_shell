737ca608e5f7dc3562a3cf6c497ed49a07217f3d
arcewp15 <970@holbertonschool.com>
2019-11-27 21:38:46 -0500

    [fix] delete lines of code, fix style of the code, the shell works

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include "shell.h"
-
+/**
+ * main - our command line interpreter
+ * Return: 0 Always Success
+ */
 int main(void)
 {
+	int cont, i;
 	char *buf;
+	char **splitBuf;
 	size_t sizebuf = 1024;
 	size_t inputchar;
-	int cont, i;
-	char **splitBuf;
 
 	buf = malloc(sizebuf * 1);
 
@@ -14,44 +17,30 @@ int main(void)
 	{
 		perror("Unable to allocate buffer"), exit(1);
 	}
-
 	while (1)
 	{
 		cont++;
 		if (isatty(STDIN_FILENO))
 		{
-			write(STDOUT_FILENO, "$ ", 2);
-			inputchar = getline(&buf, &sizebuf, stdin);
-			if(inputchar == EOF)
+			write(STDOUT_FILENO, "$ ", 2), inputchar = getline(&buf, &sizebuf, stdin);
+			if (inputchar == EOF)
 			{
-				write(STDOUT_FILENO, "\n" , 1);
-				free(buf);
-				exit(0);
+				write(STDOUT_FILENO, "\n", 1), free(buf), exit(0);
 			}
 
-			if(inputchar == 1)
+			if (inputchar == 1)
 			{
 				continue;
 			}
 
-			/*write(STDOUT_FILENO, buf, inputchar)*/;
 			splitBuf = splitInput(buf);
 			i = 0;
-			/*while(splitBuf[i] != NULL)
-			{
-				printf("%s \n",splitBuf[i]);
-				i++;
-				}*/
-			enviromentShell(splitBuf);
-			exitof(splitBuf, buf);
+			enviromentShell(splitBuf), exitof(splitBuf, buf);
 			duplicateProcess(buf, splitBuf);
-/*_error(av[0], buf);*/
 		}
 		else
-		{
-			inputchar = getline(&buf, &sizebuf, stdin);
+		{inputchar = getline(&buf, &sizebuf, stdin);
 			write(STDOUT_FILENO, buf, inputchar);
-			/*_error(av[0], buf);*/
 			break;
 		}
 	}
