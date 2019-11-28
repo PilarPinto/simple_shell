65eede8726079c5c7e6b15c5a3481e5baab13166
arcewp15 <970@holbertonschool.com>
2019-11-27 20:16:56 -0500

    [Add] prototypes of _strdup, _strcmp functions

--- shell.h
+++ shell.h
@@ -9,6 +9,8 @@
 extern char **environ;
 char *_error(char *namefile, char **buf);
 int _strlen(char *s);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
 void _prompt(void);
 char **splitInput(char *array);
 void duplicateProcess(char *array, char **space);
